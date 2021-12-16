fabb343541ce1555e377c91e38d6db355885e08d
zadon <69627510+zadon@users.noreply.github.com>
2021-12-16 11:15:10 +0800

    Create pre-install.sh

---
+++ pre-install.sh
@@ -0,0 +1,8 @@
+set -x
+sudo apt-get update -y
+sudo apt-get install -y ansible git
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -C "zadon@yandex.ru"
+cat ~/.ssh/id_rsa.pub
+git config --global user.name "Zadonsky Alexander"
+git config --global user.email "zadon@yandex.ru"
+echo 'git clone git@github.com:zadon/linuxDevOpsTools.git'
